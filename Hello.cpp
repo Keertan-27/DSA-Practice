35d5fb1a0c912d13cccb82fe2bef2461f4f9f763
Keertan Sharma <keertansharma27@gmail.com>
2026-02-10 21:28:11 +0530

    add input out

---
+++ Hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    
+    cout << "Enter the value of a: ";
+    cin >> a;
+    
+    cout << "Enter the value of b: ";
+    cin >> b;
+    
+    cout << "The sum of a: " << a << " and b: " << b << " is " << (a + b) << endl;
+    
+    return 0;
+}
