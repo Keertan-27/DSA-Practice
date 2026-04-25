81b0a3379f2cd0f819fd1406085dff04487c5005
Keertan-27 <keertansharma27@gmail.com>
2026-04-25 20:17:58 +0530

    another method using hash mapping

--- Array/appearingOnceOnly.cpp
+++ Array/appearingOnceOnly.cpp
@@ -4,7 +4,26 @@ using namespace std;
 //better solution
 int valueAppOnceOnlyBetter(vector <int> a)
 {
-    
+    int N = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        N = max(N , a[i]);
+    }
+
+    vector <int> hashh(N+1,0);
+    for (int i = 0; i < a.size(); i++)
+    {
+            hashh[a[i]]++;
+    }
+    for (int i = 0; i <= N; i++)
+    {
+        if (hashh[i] == 1)
+        {
+            return i;
+        }
+        
+    }
+    return -1;
 }
 
 // optimal solution
@@ -21,11 +40,12 @@ int valueAppearingOnceOnly(vector<int> a)
 
 int main()
 {
-    vector <int> arr = {1,1,2,2,3,4,4,8,8};
-
+    vector <int> arr = {1,1,2,2,3,3,4,4,8,8,9};
+    int ansBetter = valueAppOnceOnlyBetter(arr);
+    cout<< ansBetter << endl;
 
     int ans = valueAppearingOnceOnly(arr);
-    cout<< ans;
+    cout<< ans<< endl;
 
     return 0;
 }
