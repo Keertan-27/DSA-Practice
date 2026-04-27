900b5337ce7eebb014c68741a0edfd07e4d1bbaf
Keertan-27 <keertansharma27@gmail.com>
2026-04-28 01:39:07 +0530

    Longest subarray with sum K

--- Array/longestSubarraySumK.cpp
+++ Array/longestSubarraySumK.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
-
-int longestsubarrayWithSumK(vector<int> &arr, int Ksum)
+// worst solution O(n3)
+int longestsubarrayWithSumKWorst(vector<int> &arr, int Ksum)
 {
     int len = 0;
     int n = arr.size();
@@ -24,12 +25,87 @@ int longestsubarrayWithSumK(vector<int> &arr, int Ksum)
     return len;
 }
 
+// brut force
+int longestsubarrayWithSumKBrut(vector<int> &arr, int Ksum)
+{
+    int n = arr.size();
+    int len = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int sum = 0;
+            sum += arr[j];
+
+            if (sum == Ksum)
+            {
+                len = max(len, j - i + 1);
+            }
+        }
+    }
+    return len;
+}
+
+// better hash map
+int longestsubarrayWithSumKBetter(vector<int> &arr, long long Ksum)
+{
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+        if (sum == Ksum)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+        long long rem = sum - Ksum;
+        if (preSumMap.find(rem) != preSumMap.end())
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        if (preSumMap.find(sum) == preSumMap.end())
+        {
+            preSumMap[sum] = i;
+        }
+    }
+    return maxLen;
+}
+
+// best optimal solution
+int longestsubarrayWithSumKOptimal(vector<int> &arr, int Ksum)
+{
+    int sum = 0;
+    int maxLen = 0;
+    int i = 0, j = 0;
+    while (i < arr.size())
+    {
+        sum += arr[i];
+
+        while (sum > Ksum && j <= i)
+        {
+            sum -= arr[j];
+            j++;
+        }
+
+        if (sum == Ksum)
+        {
+            maxLen = max(maxLen, i - j + 1);
+        }
+
+        i++;
+    }
+
+    return maxLen;
+}
+
 int main()
 {
     int n = 10;
-    vector<int> arr = {1, 2, 0, 3, 4, 0, 5, 1, 1, 1};
+    vector<int> arr = {1, 2, 0, 3, 4, 0, 5, 0, 1, 1, 1};
     int k = 3;
-    int ans = longestsubarrayWithSumK(arr, 3);
+    int ans = longestsubarrayWithSumKOptimal(arr, 3);
     cout << ans;
     return 0;
 }
