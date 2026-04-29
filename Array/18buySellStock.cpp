2f2063083de9dfbce8395d7de7347a69c443fc76
Keertan-27 <keertansharma27@gmail.com>
2026-04-30 03:24:35 +0530

    buy and sell max profit array

--- Array/18buySellStock.cpp
+++ Array/18buySellStock.cpp
@@ -2,24 +2,24 @@
 #include <vector>
 using namespace std;
 // Buy and sell stock for max profit (we can only sell after buying the stock)
-int buySellStockMaxProfit(vector<int> a)
+int buySellStockMaxProfit(vector<int> prices)
 {
     int profit = 0;
-    int minValue = a[0];
-    for (int i = 0; i < a.size(); i++)
+    int minValue = prices[0];
+    for (int i = 1; i < prices.size(); i++)
     {
-        int cost = a[i] - minValue;
+        int cost = prices[i] - minValue;
         profit = max(profit, cost);
 
-        minValue = min(minValue, a[i]);
+        minValue = min(minValue, prices[i]);
     }
     return profit;
 }
 
 int main()
 {
-    vector<int> arr = {7, 1, 3, 4, 2, 6, 1, 8};
-    int ans = buySellStockMaxProfit(arr);
+    vector<int> prices = {7, 1, 3, 4, 2, 6, 1, 8};
+    int ans = buySellStockMaxProfit(prices);
     cout<< ans;
     return 0;
 }
