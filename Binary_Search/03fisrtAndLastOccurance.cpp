ad516103153e200e4611f3221b68b604010417eb
Keertan-27 <keertansharma27@gmail.com>
2026-05-15 17:09:05 +0530

    first and last ocuurance of an element

--- Binary_Search/03fisrtAndLastOccurance.cpp
+++ Binary_Search/03fisrtAndLastOccurance.cpp
@@ -50,16 +50,55 @@ pair<int, int> firstLastPosition(vector<int> arr, int x)
     return {lb, upperBound(arr, x) - 1};
 }
 
+int fistAppearance(vector <int> arr, int x)
+{
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int first = -1;
+    while (low<= high)
+    {
+        int mid = (high + low)/ 2;
+        if (arr[mid] == x)
+        {
+            first = mid;
+            high = mid-1;
+        }
+        else if(arr[mid] > x) high = mid -1;
+        else low = mid+1;
+    }
+    return first;
+}
 
+int lastAppearance(vector <int> arr, int x)
+{
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int last = -1;
+    while (low<= high)
+    {
+        int mid = (high + low)/ 2;
+        if (arr[mid] == x)
+        {
+            last = mid;
+            low = mid+1;
+        }
+        else if(arr[mid] > x) high = mid -1;
+        else low = mid+1;
+    }
+    return last;
+}
 
 int main()
 {
     vector<int> arr = {2, 5, 6, 8, 10, 10, 10, 10, 14, 16, 18, 32};
     int target = 10;
     pair<int, int> ans = firstLastPosition(arr, target);
-
+    int first_appearance = fistAppearance(arr, target);
+    int last_appearance = lastAppearance(arr, target);
     cout << "First appearance is at: " << ans.first << endl;
-    cout << "Second appearance is at: " << ans.second;
+    cout << "Second appearance is at: " << ans.second<< endl;
+    cout<< "First Appearance: "<< first_appearance << endl;
+    cout<< "Last Appearance: "<< last_appearance << endl;
 
     return 0;
 }
