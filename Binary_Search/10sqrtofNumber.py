95793f8274a4acd8d7e3ee7cf8028612307f490a
Keertan-27 <keertansharma27@gmail.com>
2026-05-17 03:45:28 +0530

    sqrt of an number

---
+++ Binary_Search/10sqrtofNumber.py
@@ -0,0 +1,15 @@
+def sqrtNumber(n):
+    low, high = 0, n
+    ans = 1
+    while(low<=high):
+        mid = (low+high) //2
+        if (mid*mid) <= n:
+            ans = mid
+            low = mid+1
+        else:
+            high = mid-1
+    return ans
+
+n = 25
+ans = sqrtNumber(n)
+print(ans)
