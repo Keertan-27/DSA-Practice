918475d96beb9f2fad0188718e4d2d97aa935450
Keertan-27 <keertansharma27@gmail.com>
2026-05-22 01:10:12 +0530

    longest substring without repeating. character

---
+++ TwoPointersSlidingWindow/02longestSubstringWithoutRepeating.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int longestSustringwithoutRepeating(string s)
+{
+    unordered_map <char, int> mp;
+    int left = 0, maxLen = 0;
+    for (int right = 0; right < s.size(); right++)
+    {
+        if (mp.find(s[right]) != mp.end() && mp[s[right]]>=left)
+            left = mp[s[right]] +1;
+
+        mp[s[right]] = right;
+        maxLen = max(maxLen, right-left+1);
+    }
+    return maxLen;
+}
+int main()
+{
+    string s = "cadbzabcd";
+    int ans = longestSustringwithoutRepeating(s);
+    cout<< ans;
+    return 0;
+}
