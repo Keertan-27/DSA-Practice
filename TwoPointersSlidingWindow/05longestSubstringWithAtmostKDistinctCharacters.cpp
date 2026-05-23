490434c63cef6dc285597df4e2d0f613ee7d777d
Keertan-27 <keertansharma27@gmail.com>
2026-05-23 21:36:39 +0530

    longest Substring With Atmost K Distinct Characters

---
+++ TwoPointersSlidingWindow/05longestSubstringWithAtmostKDistinctCharacters.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+
+int longestSubstringWithAtmostKCharacter(string s, int K)
+{
+    int right = 0, left = 0;
+    int maxLen=0;
+    map<char, int> mp;
+    while (right<s.size())
+    {
+        mp[s[right]]++;
+        if(mp.size()>K)
+        {
+            mp[s[left]]--;
+            if (mp[s[left]] == 0) 
+                mp.erase(s[left]);
+            left++;
+        }
+        if (mp.size()<=K)
+            maxLen = max(maxLen, right-left+1);
+        right++;
+    }
+    return maxLen;
+}
+
+int main()
+{
+    string s = "aaabbbccccd";
+    int ans = longestSubstringWithAtmostKCharacter(s, 2);
+    cout<<ans;
+    return 0;
+}
