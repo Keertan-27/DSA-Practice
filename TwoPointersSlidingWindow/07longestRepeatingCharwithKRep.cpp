b381e20c6b5f2fe2c4f25ddf214242788e4aed5f
Keertan-27 <keertansharma27@gmail.com>
2026-05-27 20:42:29 +0530

    longest repeating char

--- TwoPointersSlidingWindow/07longestRepeatingCharwithKRep.cpp
+++ TwoPointersSlidingWindow/07longestRepeatingCharwithKRep.cpp
@@ -18,16 +18,49 @@ int longestRepeatingCharWithKRepBrut(string s, int K)
             {
                 maxLen = max(maxLen, j - i + 1);
             }
-            else break;
+            else
+                break;
         }
     }
+    return maxLen;
+}
+
+int longestRepeatingCharWithKRepBetter(string s, int K)
+{
+    int maxLen = 0;
+    int maxfreq = 0;
+    int left = 0, right = 0;
+    int hasArray[26] = {0};
+    while (right < s.length())
+    {
+        hasArray[s[right] - 'A']++;
+        maxfreq = max(maxfreq, hasArray[s[right] - 'A']);
+
+        while ((right - left + 1) - maxfreq > K)
+        {
+            hasArray[s[left] - 'A']--;
+            maxfreq = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                maxfreq = max(maxfreq, hasArray[i]);
+                left++;
+            }
+        }
+
+        if ((right - left + 1) - maxfreq <= K)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+    }
+
     return maxLen;
 }
 
 int main()
 {
     string s = "AABABBA";
-    int ans = longestRepeatingCharWithKRepBrut(s, 2);
+    int ans = longestRepeatingCharWithKRepBetter(s, 2);
     cout << ans;
     return 0;
 }
