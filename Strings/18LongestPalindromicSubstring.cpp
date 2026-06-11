554eaad16d6abbcbd387c1cefe42ba652051678d
Keertan-27 <keertansharma27@gmail.com>
2026-06-12 01:11:52 +0530

    longest palindrome substring optimal solution

--- Strings/18LongestPalindromicSubstring.cpp
+++ Strings/18LongestPalindromicSubstring.cpp
@@ -21,10 +21,48 @@ string longestPalindromicSubstring(string s)
     }
     return longest;
 }
+
+string longestPalindromicSubstringOptimal(string s)
+{
+    int n = s.size();
+    int maxLen = 1;
+    int start = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        // odd palindrome
+        int left = i, right = i;
+        while (left>=0 && right < n && s[left] == s[right])
+        {
+            int len = right - left +1;
+            if(len > maxLen){
+                maxLen = len;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+
+        // even palindrome
+        left = i, right = i+1;
+        while (left>=0 && right < n && s[left] == s[right])
+        {
+            int len = right -left+1;
+            if (len > maxLen)
+            {
+                maxLen = len;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+    return s.substr(start, maxLen);
+}
 int main()
 {
-    string s = "babad";
-    string ans = longestPalindromicSubstring(s);
+    string s = "babbad";
+    string ans = longestPalindromicSubstringOptimal(s);
     cout<<ans;
     return 0;
 }
