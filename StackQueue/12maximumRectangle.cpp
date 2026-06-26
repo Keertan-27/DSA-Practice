23fac93f99f0cddc30669788455228ac90160dd2
Keertan-27 <keertansharma27@gmail.com>
2026-06-26 20:22:29 +0530

    maximum rectangle area 2D array

---
+++ StackQueue/12maximumRectangle.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int longestRectangle(vector<int> &heights)
+{
+    int n= heights.size();
+    stack<int> st;
+    int maxi = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        while (!st.empty() && (i == n || heights[st.top()] >= heights[i]))
+        {
+            int height = heights[st.top()];
+            st.pop();
+            int nse = i;
+            int pse = st.empty() ? -1 : st.top();
+            int width = nse - pse - 1;
+
+            maxi = max(maxi, height*width);
+        }
+        st.push(i);
+    }
+    return maxi;
+}
+int maxiRectangleInHistogram(vector<vector<char>> matrix)
+{
+    if (matrix.empty())
+        return 0;
+    
+    int row = matrix.size();
+    int col = matrix[0].size();
+    int maxArea = 0;
+    vector<int> heights(col, 0);
+    
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (matrix[i][j] == '1')
+                heights[j]++;
+            else
+                heights[j] = 0;
+        }
+        
+        maxArea = max(maxArea, longestRectangle(heights));
+    }
+    
+    return maxArea;
+}
+int main()
+{
+    vector<vector<char>> matrix = {
+        {'1', '0', '1', '0', '0'},
+        {'1', '0', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '0', '0', '1', '0'}
+    };
+    int ans = maxiRectangleInHistogram(matrix);
+    cout << ans;
+    return 0;
+}
