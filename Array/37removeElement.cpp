521df0c8301203f3b31441a10c2aef4c4a253632
Keertan-27 <keertansharma27@gmail.com>
2026-06-28 03:46:57 +0530

    remove a element from the array

---
+++ Array/37removeElement.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int removeElement(vector<int> &arr, int val)
+{
+    int i = 0;
+    for (int j = 0; j < arr.size(); j++)
+    {
+        if (arr[j] != val)
+        {
+            arr[i] = arr[j];
+            i++;
+        }
+    }
+    return i;
+}
+int main()
+{
+    vector<int> arr = {2,3,2,2,2,2,3};
+    int ans = removeElement(arr, 3);
+    cout<<ans;
+    return 0;
+}
